402a512ce7f729cf7c69b7900476e4f906daf06a
Evanjalin-K <jojaev510@gmail.com>
2024-05-26 16:03:20 +0530

    Initial Commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,13 +7,13 @@ const allItemCompleted = props.data.listData.every(list => list.completed)
   
   return (
     <div className="card">
-      <p className="plan text-muted">{props.data.plan}</p>
+      <p className="plan textblurred">{props.data.plan}</p>
         <h3 className="header">${props.data.price}/month</h3>
         <hr/>
         <div className="content">
         {
         props.data.listData.map((list, index) => (
-       <p key={index} className={list.completed ? "" : "text-muted"}>
+       <p key={index} className={list.completed ? "" : "textblurred"}>
         {list.completed ? " ✔ " : " ✖ "} {list.title} </p>
 
 )
